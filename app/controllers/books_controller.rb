0defee8eb64b88d22e431ab2617f61e8518eee7e
co2co2 <coco8287@hotmail.com>
2017-12-14 17:54:51 -0500

    done the book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,21 @@
 class BooksController < ApplicationController
   def index
-    @book = Book.all
+    @books = Book.all
 
     respond_to do |format|
       format.html
-      # format.text
-      # format.json do
-      #   render json: @books.map do |book|
-      #     {
-      #       full_name: book.name,
-      #       location: book.home,
-      #       creepiness: book.creepiness
-      #     }
-      #   end
+      format.text
+
+      format.csv { render plain: Book.generate_csv(@books) }
+      format.json do
+          render json: @books.map do |book|
+          {
+            title: book.name,
+            author: book.home,
+            already_read: book.already_read
+          }
+          end
       end
     end
+  end
 end
