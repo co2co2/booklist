877a232dabf9b787c93afa8b74ebf966d8ae37b3
co2co2 <coco8287@hotmail.com>
2017-12-14 16:48:35 -0500

    add respond to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,18 @@
 class BooksController < ApplicationController
+  def index
+    @book = Book.all
 
-
+    respond_to do |format|
+      format.html
+      # format.text
+      # format.json do
+      #   render json: @books.map do |book|
+      #     {
+      #       full_name: book.name,
+      #       location: book.home,
+      #       creepiness: book.creepiness
+      #     }
+      #   end
+      end
+    end
 end
